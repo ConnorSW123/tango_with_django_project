8c8f43ade037ef384be0d4b6d1a623b6c241d329
Connor Sweeney (student) <2880595S@student.gla.ac.uk>
2025-02-06 13:25:00 +0000

    Correcting Workspace

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from rango.models import Category, Page 
+from rango.models import PageAdmin
 
 # Register your models here.
+
+admin.site.register(Category) 
+admin.site.register(Page, PageAdmin)
+
