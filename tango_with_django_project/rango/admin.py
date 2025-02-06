1f88f59cd08b5a49c585bd86da4b69fdf1b30323
ConnorSW123 <2880595S@student.gla.ac.uk>
2025-02-06 13:53:06 +0000

    Delete tango_with_django_project directory

--- tango_with_django_project/rango/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rango.models import Category, Page 
-from rango.models import PageAdmin
-
-# Register your models here.
-
-admin.site.register(Category) 
-admin.site.register(Page, PageAdmin)
-
