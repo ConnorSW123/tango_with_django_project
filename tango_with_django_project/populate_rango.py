758ec01470c07f000963a46cbfa0fc9513ce917a
Connor Sweeney (student) <2880595S@student.gla.ac.uk>
2025-02-07 10:46:45 +0000

    Commit - Chapter 6 Done

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -30,33 +30,51 @@ def populate():
     other_frameworks_category.save()
 
     print("Categories populated!")
-
-
     python_pages =[  
         {'title': 'Official Python Tutorial',  
-         'url':'http://docs.python.org/3/tutorial/'},  
+         'url':'http://docs.python.org/3/tutorial/', 
+         "views":500},  
         {'title':'How to Think like a Computer Scientist',  
-         'url':'http://www.greenteapress.com/thinkpython/'},  
+         'url':'http://www.greenteapress.com/thinkpython/',
+         "views":300},  
         {'title':'Learn Python in 10 Minutes',  
-         'url':'http://www.korokithakis.net/tutorials/python/'} ] 
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         "views":450} 
+         ] 
     
     django_pages =[ 
         {'title':'Official Django Tutorial', 
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'}, 
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         "views":100}, 
         {'title':'Django Rocks',  
-         'url':'http://www.djangorocks.com/'}, 
+         'url':'http://www.djangorocks.com/',
+         "views":200}, 
         {'title':'How to Tango with Django',  
-         'url':'http://www.tangowithdjango.com/'} ] 
+         'url':'http://www.tangowithdjango.com/',
+         "views":400} 
+         ] 
     
     other_pages = [  
         {'title':'Bottle',  
-         'url':'http://bottlepy.org/docs/dev/'},  
+         'url':'http://bottlepy.org/docs/dev/',
+         "views":250},  
         {'title':'Flask',  
-         'url':'http://flask.pocoo.org'} ] 
-    
-    cats = { 'Python': {'pages': python_pages},  
-             'Django': {'pages': django_pages},  
-             'Other Frameworks': {'pages': other_pages} } 
+         'url':'http://flask.pocoo.org',
+         "views":350} 
+         ] 
+
+    cats = { 'Python': {'pages': python_pages,
+                        'views':128,
+                        'likes':64,
+                        },  
+            'Django': {'pages': django_pages,
+                       'views':128,
+                       'likes':64,
+                       },  
+            'Other Frameworks': {'pages': other_pages,
+                                 'views':128,
+                                 'likes':64,
+                                 }, } 
     
     # If you want to add more categories or pages, 
     # add them to the dictionaries above. 
@@ -66,14 +84,14 @@ def populate():
     for cat,cat_data in cats.items():  
         c= add_cat(cat)  
         for p in cat_data['pages']:  
-           add_page(c,p['title'], p['url']) 
+            add_page(c,p['title'], p['url'],p['views']) 
     
     # Print out the categories we have added. 
     for c in Category.objects.all(): 
         for p in Page.objects.filter(category=c): 
             print(f'- {c}: {p}') 
  
-def add_page(cat, title,url,views=0): 
+def add_page(cat, title,url,views): 
     p = Page.objects.get_or_create(category=cat,title=title)[0] 
     p.url=url 
     p.views=views 
--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from rango.models import Category, Page 
 from rango.models import PageAdmin
 
-# Register your models here.
 
-admin.site.register(Category) 
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    # Update the registration to include this customised interface
+    prepopulated_fields = {'slug':('name',)}
+
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
